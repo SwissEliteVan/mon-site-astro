5d8fc3f4116a3d34fe90caf94cd3471f8e703ec6
SwissEliteVan <214782361+SwissEliteVan@users.noreply.github.com>
2026-06-12 12:41:53 +0200

    feat: implémentation des collections de contenu Astro

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string().transform(str => new Date(str)),
+    description: z.string(),
+    author: z.string().optional(),
+    tags: z.array(z.string()).optional()
+  })
+});
+
+export const collections = {
+  blog: blogCollection
+};
