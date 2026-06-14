8cd0d7b75049901342c60b152ad0bd62fbffe61c
SwissEliteVan <214782361+SwissEliteVan@users.noreply.github.com>
2026-06-14 10:39:15 +0200

    feat: structure du blog fonctionnelle et 3 articles SEO rédigés

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,14 @@
-
 import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
-    date: z.string().transform(str => new Date(str)),
+    pubDate: z.date().or(z.string().transform(str => new Date(str))),
     description: z.string(),
     author: z.string().optional(),
-    tags: z.array(z.string()).optional()
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional()
   })
 });
 
